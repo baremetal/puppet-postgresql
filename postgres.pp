a62b8b4feeee04a37904acd7bf5496f07f0bf747
Roberto Aguilar <roberto@baremetal.io>
2012-12-30 17:17:36 -0800

    Added requirements and postgresql service.
    Require the postgresql packages to be installed before dropping the
    configuration file on disk.  Also make sure the packages are installed
    before running the service is attempted.  Finally, when the
    configuration file is dropped on disk, notify the service so it's
    restarted.

--- postgres.pp
+++ postgres.pp
@@ -4,6 +4,10 @@ Exec { path => "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin" }
 class postgres {
   $system_memory = 536870912
 
+  service { "postgresql":
+    ensure => running,
+  }
+
   exec { "update_apt":
     command => "apt-get update",
     refreshonly => true,
@@ -24,11 +28,14 @@ class postgres {
   package { ["postgresql-client-9.2", "postgresql-9.2"]:
     ensure => present,
     require => Exec["add_pitti_ppa"],
+    before => Service["postgresql"],
   }
 
   file { "/etc/postgresql/9.2/main/postgresql.conf":
     ensure => present,
     content => template("/etc/puppet/modules/postgres/templates/etc/postgresql/9.2/main/postgresql.conf"),
+    require => Package["postgresql-9.2"],
+    notify => Service["postgresql"],
   }
 
   file { "/etc/sysctl.d/30-postgresql-shm.conf":
@@ -37,5 +44,6 @@ class postgres {
     notify => Exec["notify_reboot"],
   }
 }
+
 
 include postgres
